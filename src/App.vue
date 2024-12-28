90e98bb6c6a6b58d552e52fbf5fcbef6e65f3926
daichi1998928 <daichi.tennis.928@icloud.com>
2024-12-28 17:29:06 +0900

    drag and drop で要素を描画させる

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+const handleDragStart = (event) => {
+  // ドラッグするデータを設定
+  event.dataTransfer.setData('text/plain', event.target.id);
+}
+
+const handleDragOver = (event) => {
+  // デフォルトのドラッグオーバー動作を防ぐ
+  event.preventDefault();
+}
+
+const handleDrop = (event) => {
+  // デフォルトのドロップ動作を防ぐ
+  event.preventDefault();
+  
+  // ドロップ位置を取得
+  const x = event.clientX;
+  const y = event.clientY;
+  
+  // 要素の位置を更新
+  const container = event.target;
+  container.style.position = 'absolute';
+  container.style.left = `${x}px`;
+  container.style.top = `${y}px`;
+}
+</script>
+
+<template>
+  <div 
+    class="container" 
+    draggable="true"
+    @touchstart="handleDragStart"
+    @touchmove="handleDragOver"
+    @touchend="handleDrop"
+  >
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 250px;
+  height: 250px;
+  background-color: black;
+  cursor: move; /* ドラッグ可能なことを示すカーソルを表示 */
+}
+</style>
